afa64490b280507ca5fde6856ffcb61fd853a515
Syed Shahid Ali <syedshahid1215@gmail.com>
2022-06-23 19:59:24 +0500

    complete spaceship component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
 import { useSpring, a } from "@react-spring/three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as THREE from "three"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 function App() {
   return (
-    <Canvas>
-      <Controls />
-      <Box />
-    </Canvas>
+    <>
+      {/* <h1>Hello, Shahid' Space</h1> */}
+      <Canvas camera={{ position: [0, 0, 5]}} onCreated={({ gl }) => {
+        gl.shadowMap.enabled = true
+        gl.shadowMap.type = THREE.PCFShadowMap
+      }}>
+        <fog  attach="fog" args={["black", 10, 35]} />
+        <Controls />
+        {/* <Box /> */}
+        {/* <Plane /> */}
+        <ambientLight intensity={0.5} />
+        <spotLight position={[15, 20, 5]} penumbra={1} castShadow />
+        <SpaceShip />
+      </Canvas>
+    </>
   );
 }
 
@@ -36,9 +49,13 @@ const Box = () => {
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(!active)}
       scale={props.scale}
+      castShadow
     >
+      <ambientLight />
+      <spotLight position={[0, 5, 10]} penumbra={1} castShadow />
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]}/>
-      <a.meshBasicMaterial attach="material" color={props.color} />
+      {/* <a.meshBasicMaterial attach="material" color={props.color} /> */}
+      <a.meshPhysicalMaterial attach="material" color={props.color} />
     </a.mesh>
   )
 }
@@ -54,8 +71,29 @@ const Controls = () => {
   })
   return(
     <orbitControls 
+      autoRotate
       ref={orbitRef}
       args={[camera, gl.domElement]}
+      maxPolarAngle={Math.PI / 2}
+      minPolarAngle={Math.PI / 3}
     />
   )
+}
+
+const Plane = () => (
+  <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
+    <planeBufferGeometry attach="geometry" args={[100, 100]}/>
+    <meshPhysicalMaterial attach="material" color="white" />
+  </mesh>
+)
+
+const SpaceShip = () => {
+  const [model, setModel] = useState();
+
+  useEffect(() => {
+    new GLTFLoader().load("/scene.gltf", setModel)
+  })
+
+  return model ? <primitive object={model.scene} /> : null
+  
 }
